8e2706de61425cdfac01e90060a59f3a91f6f7e6
Yohei Ueda <yohei@jp.ibm.com>
2018-09-04 15:02:37 +0900

    Fix pprof.sh
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

--- pprof.sh
+++ pprof.sh
@@ -5,24 +5,23 @@ host=localhost
 
 docker rm -f socat 2> /dev/null || true
 docker run -d --name socat --network net_ccperf --publish 7062:7062 socat tcp-listen:7062,fork,reuseaddr tcp-connect:dev-peer1.org1.example.com-ccperf-v1:6060
+trap 'docker rm -f socat' 0
 
 while sleep 5; do
     t=$(date -u +%s)
     (
-      curl -s "http://$host:7060/debug/pprof/goroutine?debug=2" > "$logdir/pprof-goroutines-orderer1.json" 
-      #curl -s "http://$host:7060/debug/pprof/block?debug=1" > "$logdir/pprof-block-orderer1.json" 
+      curl -s "http://$host:7060/debug/pprof/goroutine?debug=2" > "$logdir/pprof-goroutines-orderer1-$t.json" 
+      curl -s "http://$host:7060/debug/pprof/block?debug=1" > "$logdir/pprof-block-orderer1-$t.json" 
       #curl -s "http://$host:7060/debug/pprof/wakeup?debug=2&rate=1000" > "$logdir/pprof-wakeup-peer1-$t.json"
     ) &
     (
       curl -s "http://$host:7061/debug/pprof/goroutine?debug=2" > "$logdir/pprof-goroutines-$t.json" 
-      #curl -s "http://$host:7061/debug/pprof/block?debug=1" > "$logdir/pprof-block-peer1-$t.json" 
+      curl -s "http://$host:7061/debug/pprof/block?debug=1" > "$logdir/pprof-block-peer1-$t.json" 
       #curl -s "http://$host:7061/debug/pprof/wakeup?debug=2&rate=1000" > "$logdir/pprof-wakeup-peer1-$t.json" 
     ) &
     (
       curl -s "http://$host:7062/debug/pprof/goroutine?debug=2" > "$logdir/pprof-goroutines-chaincode-$t.json" 
-      #curl -s "http://$host:7062/debug/pprof/block?debug=1" > "$logdir/pprof-block-chaincode.json" 
+      curl -s "http://$host:7062/debug/pprof/block?debug=1" > "$logdir/pprof-block-chaincode.json" 
       #curl -s "http://$host:7062/debug/pprof/wakeup?debug=2&rate=1000" > "$logdir/pprof-chaincode-goroutines-$t.json"
     ) &
 done
-
-docker rm -f socat
