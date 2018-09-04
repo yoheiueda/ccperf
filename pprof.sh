331c921deff9c2fab1dc48729f2d2ceff0720df5
Yohei Ueda <yohei@jp.ibm.com>
2018-09-04 13:18:12 +0900

    Add socat Dockerfile
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

--- pprof.sh
+++ pprof.sh
@@ -4,7 +4,7 @@ logdir=${1:-./logs}
 host=localhost
 
 docker rm -f socat 2> /dev/null || true
-docker run -d --name socat --network net_ccperf --publish 7062:7062 alpine/socat tcp-listen:7062,fork,reuseaddr tcp-connect:dev-peer1.org1.example.com-ccperf-v1:6060
+docker run -d --name socat --network net_ccperf --publish 7062:7062 socat tcp-listen:7062,fork,reuseaddr tcp-connect:dev-peer1.org1.example.com-ccperf-v1:6060
 
 while sleep 5; do
     t=$(date -u +%s)
@@ -15,7 +15,7 @@ while sleep 5; do
     ) &
     (
       curl -s "http://$host:7061/debug/pprof/goroutine?debug=2" > "$logdir/pprof-goroutines-$t.json" 
-      curl -s "http://$host:7061/debug/pprof/block?debug=1" > "$logdir/pprof-block-peer1-$t.json" 
+      #curl -s "http://$host:7061/debug/pprof/block?debug=1" > "$logdir/pprof-block-peer1-$t.json" 
       #curl -s "http://$host:7061/debug/pprof/wakeup?debug=2&rate=1000" > "$logdir/pprof-wakeup-peer1-$t.json" 
     ) &
     (
