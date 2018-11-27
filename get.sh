f861b3b962f329048db6d0f63c1bf212b1df03da
Yohei Ueda <yohei@jp.ibm.com>
2018-11-27 17:48:08 +0900

    Support Fabric 1.3
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

--- get.sh
+++ get.sh
@@ -2,8 +2,8 @@
 
 os=$(go env GOOS)
 arch=$(go env GOARCH)
-rel=1.2.0
+rel=1.3.0
 url="https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/$os-$arch-$rel/hyperledger-fabric-$os-$arch-$rel.tar.gz"
 
 [ -d ./bin ] || curl "$url" | tar xzf - bin
-[ -e core.yaml ] || curl -O -L https://raw.githubusercontent.com/hyperledger/fabric/release-1.2/sampleconfig/core.yaml
+[ -e core.yaml ] || curl -O -L https://raw.githubusercontent.com/hyperledger/fabric/release-1.3/sampleconfig/core.yaml
