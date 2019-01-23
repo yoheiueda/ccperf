845d784f8ae377f4b49b9476576780484e5a4675
Yohei Ueda <yohei@jp.ibm.com>
2019-01-23 17:19:20 +0900

    Add tinyproxy
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

--- get.sh
+++ get.sh
@@ -2,6 +2,10 @@
 
 os=$(go env GOOS)
 arch=$(go env GOARCH)
+
 . ./.env
-[ -d ./bin ] || mkdir -p bin && docker run --rm hyperledger/fabric-tools:$FABRIC_TAG tar -cf - -C /usr/local/bin configtxgen configtxlator cryptogen discover idemixgen peer | tar -xvf - -C ./bin
-[ -e core.yaml ] || docker run --rm hyperledger/fabric-tools:$FABRIC_TAG cat /etc/hyperledger/fabric/core.yaml > core.yaml
+
+mkdir -p bin && docker run --rm hyperledger/fabric-tools:$FABRIC_TAG tar -cf - -C /usr/local/bin configtxgen configtxlator cryptogen discover idemixgen peer | tar -xvf - -C ./bin
+docker run --rm hyperledger/fabric-tools:$FABRIC_TAG cat /etc/hyperledger/fabric/core.yaml > core.yaml
+
+[ $(docker image ls -q tinyproxy) == "" ] && (cd dockerfiles/tinyproxy && ./build.sh)
