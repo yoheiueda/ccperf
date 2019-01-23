dfa5b96be8872849e03d95668bdb8a456a393b08
Yohei Ueda <yohei@jp.ibm.com>
2019-01-23 18:20:31 +0900

    Add pprof and FlameGraph
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

--- env.sh
+++ env.sh
@@ -1,2 +1,16 @@
 
-export GOPATH=$PWD/go
+export GOPATH="$PWD/go"
+
+. .env
+
+if ! [[ ":$PATH:" =~ ":$PWD/bin:" ]]; then
+  export PATH="$PWD/bin:$PATH"
+fi
+
+if ! [[ ":$PATH:" =~ ":$PWD/bin/FlameGraph:" ]]; then
+  export PATH="$PWD/bin/FlameGraph:$PATH"
+fi
+
+if ! [[ ":$PATH:" =~ ":$GOPATH/bin:" ]]; then
+  export PATH="$GOPATH/bin:$PATH"
+fi
--- get.sh
+++ get.sh
@@ -3,9 +3,17 @@
 os=$(go env GOOS)
 arch=$(go env GOARCH)
 
-. ./.env
+. ./env.sh
 
-mkdir -p bin && docker run --rm hyperledger/fabric-tools:$FABRIC_TAG tar -cf - -C /usr/local/bin configtxgen configtxlator cryptogen discover idemixgen peer | tar -xvf - -C ./bin
+mkdir -p bin
+mkdir -p go/bin
+
+go get -u github.com/google/pprof
+go get -u github.com/uber/go-torch
+
+[ -d "$PWD/FlameGraph" ] || git clone https://github.com/brendangregg/FlameGraph.git bin/FlameGraph
+
+docker run --rm hyperledger/fabric-tools:$FABRIC_TAG tar -cf - -C /usr/local/bin configtxgen configtxlator cryptogen discover idemixgen peer | tar -xvf - -C ./bin
 docker run --rm hyperledger/fabric-tools:$FABRIC_TAG cat /etc/hyperledger/fabric/core.yaml > core.yaml
 
 [ $(docker image ls -q tinyproxy) == "" ] && (cd dockerfiles/tinyproxy && ./build.sh)
