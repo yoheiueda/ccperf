9d2922f54f35f04a8b38378b6e6c92a89f544758
Yohei Ueda <yohei@jp.ibm.com>
2019-01-24 12:37:28 +0900

    Get prebuilt binaries for Mac
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

--- get.sh
+++ get.sh
@@ -11,9 +11,19 @@ mkdir -p go/bin
 go get -u github.com/google/pprof
 go get -u github.com/uber/go-torch
 
-[ -d "$PWD/FlameGraph" ] || git clone https://github.com/brendangregg/FlameGraph.git bin/FlameGraph
+[ -d "bin/FlameGraph" ] || git clone https://github.com/brendangregg/FlameGraph.git bin/FlameGraph
 
-docker run --rm hyperledger/fabric-tools:$FABRIC_TAG tar -cf - -C /usr/local/bin configtxgen configtxlator cryptogen discover idemixgen peer | tar -xvf - -C ./bin
+case "$os" in
+  darwin)
+    rel=1.4.0
+    url="https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/$os-$arch-$rel/hyperledger-fabric-$os-$arch-$rel.tar.gz"
+    curl "$url" | tar xzf - bin
+    ;;
+  *)
+    docker run --rm hyperledger/fabric-tools:$FABRIC_TAG tar -cf - -C /usr/local/bin configtxgen configtxlator cryptogen discover idemixgen peer | tar -xvf - -C ./bin
+    ;;
+esac
+
 docker run --rm hyperledger/fabric-tools:$FABRIC_TAG cat /etc/hyperledger/fabric/core.yaml > core.yaml
 
 [ $(docker image ls -q tinyproxy) == "" ] && (cd dockerfiles/tinyproxy && ./build.sh)
