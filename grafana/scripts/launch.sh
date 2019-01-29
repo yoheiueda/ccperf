c8aeace67328419508237c2c85bc53f0943cc336
Yohei Ueda <yohei@jp.ibm.com>
2019-01-29 11:28:48 +0900

    Update Grafana dashboards
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

--- grafana/scripts/launch.sh
+++ grafana/scripts/launch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+dashboard_uid=fabric1
+
 /run.sh "$@" &
 sleep 5
-curl -q -H 'Accept: application/json' -H 'Content-type: application/json' -X PUT --data '{ "homeDashboardId": 1 }' --user "admin:$GF_SECURITY_ADMIN_PASSWORD" 'http://localhost:3000/api/org/preferences'
+id=$(curl -s -H 'Accept: application/json' -X GET --user "admin:$GF_SECURITY_ADMIN_PASSWORD" "http://localhost:3000/api/dashboards/uid/$dashboard_uid" | grep -Po '"dashboard":.*?"id":\K\d+')
+curl -q -H 'Accept: application/json' -H 'Content-type: application/json' -X PUT --data "{ \"homeDashboardId\": $id }" --user "admin:$GF_SECURITY_ADMIN_PASSWORD" 'http://localhost:3000/api/org/preferences'
 wait
