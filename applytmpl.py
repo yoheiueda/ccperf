8422101fc6fd2cc09175ae7acd52e4de33925750
Yohei Ueda <yohei@jp.ibm.com>
2019-06-12 11:08:00 +0900

    Support embedded keys and certs
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

--- applytmpl.py
+++ applytmpl.py
@@ -16,8 +16,10 @@ else:
     genJSON = False
     tplfile = sys.argv[1]
 
+
 def getip(host):
     return socket.gethostbyname(host)
+
 
 def findsk(org):
     dir = 'crypto-config/peerOrganizations/' + \
@@ -25,8 +27,19 @@ def findsk(org):
     return glob.glob(dir + '/*_sk')[0]
 
 
+def readpem(path):
+    with open(path) as f:
+        pem = '\"'
+        for line in f.read().splitlines():
+            pem += line + '\\n'
+        pem += '\"'
+        return pem
+
+
 env.filters['getip'] = getip
 env.filters['findsk'] = findsk
+env.filters['readpem'] = readpem
+
 tpl = env.get_template(tplfile)
 
 commonDomain = 'example.com'
@@ -34,15 +47,20 @@ commonHostname = 'localhost'
 
 data = {
     'network': 'ccperf',
+    'embedKeys': True,
+    'client': {
+        'org': 'org1'
+    },
     'ordererorg': {
         'name': 'orderer',
         'mspid': 'OrdererOrg',
         'domain': commonDomain,
-        #'type': 'etcdraft',
-        #'type': 'kafka',
+        # 'type': 'etcdraft',
+        # 'type': 'kafka',
         'type': 'solo',
         'orderers': [
-            {'name': 'orderer1', 'host': commonHostname, 'ports': {'requests': 7050, 'pprof': 7060}, 'volume':'tmpfs' },
+            {'name': 'orderer1', 'host': commonHostname, 'ports': {
+                'requests': 7050, 'pprof': 7060}, 'volume': 'tmpfs'},
         ]
     },
     'orgs': [
@@ -52,22 +70,23 @@ data = {
             'domain': 'org1.' + commonDomain,
             'useLeaderElection': True,
             'peers': [
-                {'name': 'peer1', 'ports': {'requests': 7051, 'pprof': 7061 }, 'host': commonHostname, 'bootstrap': 'peer1', 'volume':'tmpfs' },
+                {'name': 'peer1', 'ports': {'requests': 7051, 'pprof': 7061},
+                    'host': commonHostname, 'bootstrap': 'peer1', 'volume': 'tmpfs'},
             ]
-         }
+        }
     ],
     'cadvisors': [
-         {'host':'cadvisor', 'port':8080},
-     ],
+        {'host': 'cadvisor', 'port': 8080},
+    ],
     'node_exporters': [
-         {'host':'node_exporter', 'port':9100},
-     ],
+        {'host': 'node_exporter', 'port': 9100},
+    ],
     'grafana': {
         'port': 3000
     },
     'prometheus': {
         'port': 9090,
-        #'volume': '/var/tmp/prometheus',
+        # 'volume': '/var/tmp/prometheus',
     },
     'pushgateway': {
         'port': 9091
