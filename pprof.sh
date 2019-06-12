368f9402d63b44178cb719911cc44e37ee973f38
Yohei Ueda <yohei@jp.ibm.com>
2019-06-12 10:32:43 +0900

    Update pprof.sh
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

--- pprof.sh
+++ pprof.sh
@@ -1,25 +1,56 @@
 #!/bin/bash
 
-logdir=${1:-./logs}
+seconds=${1:-60}
+label=${2:-pprof}
+logdir=${3:-./logs}
+interval=5
 host=localhost
-
+list='orderer1 org1-peer1 dev-peer1.org1.example.com-ccperf-v1'
 export http_proxy="http://$host:8888"
 
-while sleep 5; do
+collect_start() {
     t=$(date -u +%s)
-    (
-      curl -s "http://org1-peer1:6060/debug/pprof/goroutine?debug=2" > "$logdir/pprof-goroutines-peer1-$t.json" 
-      curl -s "http://org1-peer1:6060/debug/pprof/block?debug=1" > "$logdir/pprof-block-peer1-$t.json" 
-      curl -s "http://org1-peer1:6060/debug/pprof/wakeup?debug=2&rate=1000" > "$logdir/pprof-wakeup-peer1-$t.json"
-    ) &
-    (
-      curl -s "http://orderer1:6060/debug/pprof/goroutine?debug=2" > "$logdir/pprof-goroutines-orderer1-$t.json" 
-      curl -s "http://orderer1:6060/debug/pprof/block?debug=1" > "$logdir/pprof-block-orderer1-$t.json" 
-      curl -s "http://orderer1:6060/debug/pprof/wakeup?debug=2&rate=1000" > "$logdir/pprof-wakeup-orderer1-$t.json" 
-    ) &
-    (
-      curl -s "http://dev-peer1.org1.example.com-ccperf-v1:6060/debug/pprof/goroutine?debug=2" > "$logdir/pprof-goroutines-chaincode-$t.json" 
-      curl -s "http://dev-peer1.org1.example.com-ccperf-v1:6060/debug/pprof/block?debug=1" > "$logdir/pprof-block-chaincode-$t.json" 
-      curl -s "http://dev-peer1.org1.example.com-ccperf-v1:6060/debug/pprof/wakeup?debug=2&rate=1000" > "$logdir/pprof-wakeup-chaincode-$t.json"
-    ) &
+
+    for target in $list; do
+      (
+        curl -s "http://$target:6060/debug/pprof/wakeup?debug=2&rate=1000" > "$logdir/$label-wakeup-$target-$t.json"
+        curl -s "http://$target:6060/debug/pprof/wakeup" > "$logdir/$label-wakeup-$target-$t.pb"
+        curl -s "http://$target:6060/debug/pprof/block?seconds=$seconds&rate=1000" > "$logdir/$label-block-$target.pb" &
+        curl -s "http://$target:6060/debug/pprof/profile?seconds=$seconds" > "$logdir/$label-cpu-$target.pb" &
+      ) &
+    done
+}
+
+collect_end() {
+    t=$(date -u +%s)
+    for target in $list; do
+      (
+        curl -s "http://$target:6060/debug/pprof/wakeup?debug=2" > "$logdir/$label-wakeup-$target-$t.json"
+        curl -s "http://$target:6060/debug/pprof/wakeup" > "$logdir/$label-wakeup-$target-$t.pb"
+        #curl -s "http://$target:6060/debug/pprof/block" > "$logdir/$label-block-$target-$t.pb" 
+      ) &
+    done
+}
+
+collect_start
+
+while [[ $seconds > 0 ]] && [[ -z "$finish" ]] ; do
+    t=$(date -u +%s)
+    for target in $list; do
+      curl -s "http://org1-peer1:6060/debug/pprof/goroutine?debug=2" > "$logdir/$label-goroutines-$target-$t.json"  &
+    done
+    (( seconds -= $interval ))
+    sleep $interval || break
+done
+
+collect_end
+
+wait
+
+for target in $list; do
+    if [ -x ./go-bottleneck ]; then
+        ./go-bottleneck "$logdir"/$label-goroutines-$target-*.json "$logdir"/$label-wakeup-$target-*.json > "$logdir"/$label-$target.dot
+        dot -Tpdf "$logdir"/$label-$target.dot > "$logdir"/$label-$target.pdf
+    fi
+    [ -d bin/FlameGraph ] && go tool pprof -raw "$logdir/$label-cpu-$target.pb" | bin/FlameGraph/stackcollapse-go.pl | bin/FlameGraph/flamegraph.pl > "$logdir/$label-cpu-$target.svg"
 done
